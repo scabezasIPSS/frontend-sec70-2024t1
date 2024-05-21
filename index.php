ad0a7ae2055103eca3163db1e70eb8534d84a36b
scabezasciisa <scabezas@ciisa.cl>
2024-05-20 21:03:32 -0400

    dev: 14-05-2024

--- index.php
+++ index.php
@@ -6,22 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Frontend 70</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        footer {
+            color: white;
+            background-color: black;
+        }
+    </style>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary">
         <div class="container">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">ConIngenio.cl</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        <a class="nav-link" href="#">Features</a>
-                        <a class="nav-link" href="#">Pricing</a>
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                        <a class="nav-link" href="#nosotros">Nosotros</a>
+                        <a class="nav-link" href="#servicios">Servicios</a>
+                        <a class="nav-link" href="#contacto">Contáctenos</a>
                     </div>
                 </div>
             </div>
@@ -55,7 +61,7 @@
         </button>
     </div>
 
-    <section>
+    <section id="nosotros">
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
@@ -72,9 +78,10 @@
         </div>
     </section>
 
-    <section>
+    <section id="servicios">
         <div class="container">
-            <div class="row">
+            <h2>Servicios</h2>
+            <div id="rowServicios" class="row">
                 <div class="col-md-3">
                     <div class="card">
                         <div class="card-header">Consultoría digital</div>
@@ -103,7 +110,177 @@
         </div>
     </section>
 
+    <section id="contacto">
+        <div class="container">
+            <h2>Contáctenos</h2>
+            <div class="row">
+                <div class="col-md-6">
+                    <div>
+                        <label id="lblMensaje" class="d-none">Error? exitoso?</label>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="fi_nombre" placeholder="Ingrese su nombre">
+                            <label for="fi_nombre">Nombre</label>
+                        </div>
+                        <div class="form-floating">
+                            <select class="form-select" id="fs_servicios" aria-label="Servicios">
+                                <option value=0 selected>Seleccione un servicio</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                            <label for="fs_servicios">Seleccione un servicio</label>
+                        </div>
+                        <div class="form-floating">
+                            <textarea class="form-control" placeholder="Ingrese su mensaje aquí" style="height: 100px" id="fta_mensaje"></textarea>
+                            <label for="fta_mensaje">Mensaje</label>
+                        </div>
+                        <input type="checkbox" class="form-check-input" id="checkEnvio" value="checkeado">
+                        <label for="checkEnvio">¿Exitoso?</label>
+                        <button class="btn btn-primary" onclick="validarFormulario(this)">Enviar</button>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    imagen de google maps
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">asd
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                </div>
+                <div class="col-md-3"></div>
+                <div class="col-md-3"></div>
+            </div>
+        </div>
+
+    </footer>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script>
+        // fetch('assets/json/servicio.json')
+        fetch('http://localhost/backend-sec70-2024t1/v1/services/', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ciisa',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(respuesta => {
+                //.ok es una forma de simular la forma del API, por eso lo tenemos como variable
+                if (!respuesta.ok) {
+                    throw new Error('Error al recuperar');
+                }
+                return respuesta.json();
+            })
+            .then(salida => {
+                console.log(salida.data);
+                const anchoColumnas = 12 / salida.data.length;
+                const rowServicios = document.getElementById('rowServicios');
+                rowServicios.innerHTML = '';
+                salida.data.forEach(element => {
+                    const columna = document.createElement('div');
+                    columna.classList.add('col-md-' + anchoColumnas);
+                    const tarjeta = document.createElement('div');
+                    tarjeta.classList.add('card');
+                    const tarjetaHeader = document.createElement('div');
+                    tarjetaHeader.classList.add('card-header');
+                    const tarjetaBody = document.createElement('div');
+                    tarjetaBody.classList.add('card-body');
+                    const tarjetaFooter = document.createElement('div');
+                    tarjetaFooter.classList.add('card-footer');
+                    //trabajo con los datos
+                    tarjetaHeader.innerText = element.titulo.esp;
+                    tarjetaBody.innerText = element.texto.esp;
+                    // console.log(element.texto.esp);
+                    tarjetaFooter.innerHTML = '<a href="#contacto"><button onclick="cambiaServicio(`'+element.id+'`)">Contáctanos</button></a>';
+                    tarjeta.appendChild(tarjetaHeader);
+                    tarjeta.appendChild(tarjetaBody);
+                    tarjeta.appendChild(tarjetaFooter);
+                    columna.appendChild(tarjeta);
+                    rowServicios.appendChild(columna);
+                });
+
+                console.log('cantidad columnas: ' + anchoColumnas);
+                console.log(rowServicios);
+            })
+            .catch(error => {
+                console.error('Error al obtener: ', error);
+            });
+
+        function validarFormulario(_boton) {
+            let validos = false;
+            const lblMensaje = document.getElementById('lblMensaje');
+            const input_nombre = document.getElementById('fi_nombre');
+            const select_servicios = document.getElementById('fs_servicios');
+            const textArea_mensaje = document.getElementById('fta_mensaje');
+            const check_envio = document.getElementById('checkEnvio');
+            console.log(_boton);
+            console.log(input_nombre);
+            console.log(select_servicios);
+            console.log(textArea_mensaje);
+            console.log(check_envio);
+
+            lblMensaje.innerText = 'Error:';
+
+            // Valido que el input nombre contenta caracteres
+            if (input_nombre.value.length == 0) {
+                input_nombre.classList.add('is-invalid');
+                validos = false;
+                lblMensaje.innerText += ' falta nombre /';
+            } else {
+                //validar que caracteres son válidos.
+                input_nombre.classList.remove('is-invalid');
+                input_nombre.classList.add('is-valid');
+                validos = true;
+            }
+
+            if (select_servicios.value == 0) {
+                select_servicios.classList.add('is-invalid');
+                validos &= false;
+                lblMensaje.innerText += ' falta servicio /';
+            } else {
+                select_servicios.classList.remove('is-invalid');
+                select_servicios.classList.add('is-valid');
+                validos &= true;
+            }
+
+            if (textArea_mensaje.value.length == 0) {
+                textArea_mensaje.classList.add('is-invalid');
+                validos &= false;
+                lblMensaje.innerText += ' falta mensaje /';
+            } else {
+                textArea_mensaje.classList.remove('is-invalid');
+                textArea_mensaje.classList.add('is-valid');
+                validos &= true;
+            }
+
+            if (!validos) {
+                lblMensaje.classList.remove('d-none');
+            } else {
+                lblMensaje.classList.add('d-none');
+            }
+
+            if (check_envio.value == 'checkeado') {
+                console.log('checkeado');
+            } else {
+                console.log('No checkeado');
+            }
+
+        }
+
+        function cambiaServicio(_llega){
+            alert(_llega);
+        }
+    </script>
 </body>
 
 </html>
